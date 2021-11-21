64f386e399eac7cc933509d305c00a49cb22987a
Taiwo Oluwaseun <taiwoluwaseun8@gmail.com>
2021-11-21 09:22:51 +0100

    controllers

---
+++ src/teacher/student.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Put } from "@nestjs/common";
+
+@Controller('teachers/:teacherId/students')
+export class StudentTeacherController {
+    @Get()
+    getStudents() {
+        return "Get All Students That Belong To A Teacher"
+    }
+
+    @Put('/:studentId')
+    updateStudentTeacher() {
+        return "Update Student Teacher"
+    }
+}
+
