81a8df89d6909eb4e6b394a55686b5a6f5064005
Taiwo Oluwaseun <taiwoluwaseun8@gmail.com>
2021-11-21 12:10:19 +0100

    data transfer objects

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,23 +1,29 @@
 import { Body, Controller, Get, Param, Post, Put } from "@nestjs/common";
+import { 
+    CreateStudentDto, 
+    UpdateStudentDto, 
+    FindStudentResponseDto, 
+    StudentResponseDto 
+} from "./dto/student.dto";
 
 @Controller('students')
 export class StudentController {
     @Get()
-    getStudents() {
+    getStudents(): FindStudentResponseDto[] {
         return "All Students"
     }
 
     @Get('/:studentId')
     getStudentById(
         @Param('studentId') studentId: string
-    ) {
+    ): FindStudentResponseDto {
         return `Get Student With Id of ${studentId}`
     }
 
     @Post()
     createStudent(
-        @Body() body
-    ) {
+        @Body() body: CreateStudentDto
+    ): StudentResponseDto {
         console.log(body);
         return `Create Student With The Following Data ${JSON.stringify(body)}`
     }
@@ -25,8 +31,8 @@ export class StudentController {
     @Put('/:studentId')
     updateStudent(
         @Param('studentId') studentId: string,
-        @Body() body
-    ) {
+        @Body() body: UpdateStudentDto
+    ): StudentResponseDto {
         return `Update Student With Id of ${studentId} With Data of ${JSON.stringify(body)}`
     }
 }
--- src/teacher/student.controller.ts
+++ src/teacher/student.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get, Param, Put } from "@nestjs/common";
+import { FindStudentResponseDto, StudentResponseDto } from "../student/dto/student.dto";
 
 @Controller('teachers/:teacherId/students')
 export class StudentTeacherController {
     @Get()
     getStudents(
         @Param('teacherId') teacherId: string
-    ) {
+    ): FindStudentResponseDto[] {
         return `Get All Students That Belong To The Teacher With An Id Of ${teacherId}`
     }
 
@@ -13,8 +14,7 @@ export class StudentTeacherController {
     updateStudentTeacher(
         @Param('teacherId') teacherId: string,
         @Param('StudentId') studentId: string
-    ) {
+    ): StudentResponseDto {
         return `Update Student With Id of ${studentId} To Teacher With Id of ${teacherId}`
     }
 }
-
